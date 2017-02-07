fd3cdd2cd6c7a1795f505c61bedd4de4ef68f39d
vagrant <vagrant@vagrant.vm>
2017-02-07 05:57:12 +0000

    people able to draw polygon on the map

--- server.py
+++ server.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from constant import TOKEN, APP_SECRETE
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
 
 app.secret_key = APP_SECRETE
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
     """Home Page"""
 
     return render_template("map4.html", token=TOKEN)
 
+@app.route("/", methods=['POST'])
+def dis_restaurants():
+    """Display the restaurants within the polygon region"""
+    print "expecting array of array"
+    # raise Exception 
+    print request.form.get('features')
+    # raise Exception 
+    return jsonify({"result":"the result"})
+    # polygon_points = re /quest.form.get("")
+
+
 if __name__ == "__main__":
     app.debug = True
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
     app.run(port=5000, host='0.0.0.0')
+
