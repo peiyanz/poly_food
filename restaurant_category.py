1f7c089e6c7e86c2986df0c39398cfa580c9c78b
peiyan zhao <peiyan@py.local>
2017-02-17 01:06:07 -0800

    add restaurant database

---
+++ restaurant_category.py
@@ -0,0 +1,183 @@
+#categories:
+
+
+category = {"afghani": "afghani",
+            "african": "african",
+            "senegalese": "african",
+            "southafrican": "african",
+            "newamerican":"newamerican",
+            "tradamerican":"tradamerican",
+            "arabian":"arabian",
+            "armenian": "armenian",
+            "argentine": "argentine",
+            "asianfusion":"asianfusion",
+            "australian": "australian",
+            "austrian": "australian",
+            "bangladeshi":"bangladeshi",
+            "bbq":"tradamerican",
+            "basque":"basque",
+            "belgian":"belgian",
+            "brasseries":"french",
+            "brazilian": "brazilian",
+            "breakfast_brunch": "tradamerican",
+            "british":"british",
+            "buffets": "seafood",
+            "burgers": "tradamerican",
+            "burmese":"burmese",
+            "cafes":"cafes",
+            "themedcafes": "cafes",
+            "cafeteria": "cafes",
+            "cajun": "cajun",
+            "cambodian":"cambodian",
+            "caribbean":"caribbean",
+            "dominican": "caribbean",
+            "haitian": "haitian",
+            "puertorican": "caribbean",
+            "trinidadian": "caribbean",
+            "catalan": "french",
+            "cheesesteaks":"tradamerican",
+            "chickenshop":"tradamerican",
+            "chicken_wings":"tradamerican",
+            "chinese":"chinese",
+            "cantonese":"chinese",
+            "dimsum":"chinese",
+            "hainan":"chinese",
+            "shanghainese": "chinese",
+            "szechuan":"chinese",
+            "comfortfood": "newamerican",
+            "creperies": "cafes",
+            "cuban":"cuban",
+            "czech": "czech",
+            "delis":"fastfood",
+            "diners":"dinner",
+            "dinnertheater":"dinner",
+            "ethiopian":"african",
+            "hotdogs": "fastfood",
+            "filipino":"filipino",
+            "fishnchips":"seafood",
+            "foodstands": "fastfood",
+            "fondue": "newamerican",
+            "food_court": "fastfood",
+            "french":"french",
+            "mauritius":"african",
+            "reunion": "dinner",
+            "gastropubs": "newamerican",
+            "german": "german",
+            "gluten_free": "newamerican",
+            "greek":"greek",
+            "guamanian":"guamanian",
+            "halal":"italian",
+            "hawaiian":"hawaiian",
+            "himalayan":"indian",
+            "honduran":"honduran",
+            "hkcafe":"chinese",
+            "hotdog":"fastfood",
+            "hotpot":"chinese",
+            "hungarian":"hungarian",
+            "iberian":"african",
+            "indpak":"indian",
+            "indonesian":"indonesian",
+            "irish":"irish",
+            "italian":"italian",
+            "calabrian":"italian",
+            "sardinian":"mediterranean",
+            "tuscan":"italian",
+            "japanese":"japanese",
+            "conveyorsushi":"japanese",
+            "izakaya":"japanese",
+            "ramen":"japanese",
+            "teppanyaki":"japanese",
+            "kebab": "indian",
+            "korean":"korean",
+            "kosher":"fastfood",
+            "laotian":"laotian",
+            "latin":"latin",
+            "colombian":"colombian",
+            "salvadoran": "caribbean",
+            "venezuelan":"caribbean",
+            "raw_food":"seafood",
+            "malaysian":"malaysian",
+            "mediterranean":"mediterranean",
+            "falafel":"greek",
+            "mexican":"mexican",
+            "tacos":"mexican",
+            "mideastern":"mediterranean",
+            "egyptian":"african",
+            "seafood":"seafood",
+            "sushi":"japanese",
+            "noodles": "japanese",
+            "newmexican":"mexican",
+            "mongolian":"chinese",
+            "taiwanese":"chinese",
+            "tapas":"italian",
+            "tapasmallplates":"italian",
+            "thai":"thai",
+            "turkish":"turkish",
+            "ukrainian":"ukrainian",
+            "steak":"tradamerican",
+            "singaporean":"chinese",
+            "lebanese":"arabian",
+            "moroccan":"moroccan",
+            "uzbek":"asianfusion",
+            "tex-mex":"mexican",
+            "syrian":"syrian",
+            "supperclubs": "newamerican",
+            "nicaraguan":"caribbean",
+            "pakistani":"syrian",
+            "panasian":"asianfusion",
+            "persian":"persian",
+            "peruvian":"peruvian",
+            "polish":"polish",
+            "popuprestaurants": "newamerican",
+            "spanish":"spanish",
+            "portuguese":"portuguese",
+            "poutineries":"fastfood",
+            "russian":"russian",
+            "scandinavian":"scottish",
+            "scottish":"scottish",
+            "slovakian":"italian",
+            "soulfood":"fastfood",
+            "soup": "italian",
+            "southern": "latin",
+            "srilankan":"indian",
+            "modern_european":"german",
+            "pizza": "italian",
+            "sandwiches":"fastfood",
+            "salad": "vegetarian",
+            "vegan":"vegetarian",
+            "vegetarian":"vegetarian",
+            "vietnamese":"vietnamese",
+            "waffles":"tradamerican",
+            "wraps":"fastfood"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             }
--- yelp_api_v3.py
+++ yelp_api_v3.py
@@ -2,12 +2,15 @@ from yelp.api.v3 import Yelp
 from constant import app_id, app_secret
 import pandas as pd
 import time
+from sqlalchemy import create_engine
 
 
 yelp = Yelp(
     app_id,
     app_secret,
 )
+engine = create_engine('postgresql://peiyan:peiyan@localhost:8000/peiyan')
+existing_data = pd.read_sql_query('SELECT * FROM restaurants',con=engine)
 
 # Simple Examples
 # print yelp.search(term='food', latitude=37.773972, longitude=-122.431297, radius=1000, limit=1)
@@ -18,7 +21,7 @@ yelp = Yelp(
 def api_call(latitude, longitude, radius, offset):
     time1 = time.time()
     # results = yelp.search(term='food', latitude=37.773972, longitude=-122.431297, radius=1000, offset=offset,limit=1)
-    results = yelp.search(term='restaurants', latitude=latitude, longitude=longitude, radius=radius, offset=offset,limit=50)
+    results = yelp.search(term='restaurants', latitude=latitude, longitude=longitude, radius=radius, offset=offset+950,limit=50)
     if results.get("total") > 0:
         print "total"
         print results.get("total")
@@ -32,8 +35,10 @@ def api_call(latitude, longitude, radius, offset):
         url = []
         image = []
         name = []
+        id = []
         # for r in results:#, restaurants3, restaurants4, restaurants5]:
         for i in results.get('businesses'):
+            id.append(i.get('id'))
             category.append(i.get('categories')[0].get("alias") )
             location.append(i.get('location').get("display_address"))
             longitude.append(i.get('coordinates').get('longitude'))
@@ -46,12 +51,22 @@ def api_call(latitude, longitude, radius, offset):
             name.append(i.get('name'))
 
 
-        rest_info = pd.DataFrame({"name": name, "category": category, "address": location, "latitude": latitude, "longitude": longitude,
+        rest_info = pd.DataFrame({"id": id, "name": name, "category": category, "address": location, "latitude": latitude, "longitude": longitude,
                       "stars": rating, "price": price, "review_count": review_count, "url": url, "image": image})
         # print rest_info
         time2 = time.time()
         print '%s function took %0.3f ms' % ("rip", (time2-time1)*1000.0)
 
+        #rest_info is the new data from api call
+        
+        new_data = rest_info[~rest_info["id"].isin(existing_data["id"].tolist())]
+        # print "this is new data"
+        # print new_data
+        existing_data.append(new_data)
+        # print existing_data
+        data = new_data.set_index("id")
+        data.to_sql('restaurants', engine, if_exists='append')                    
+        
         return rest_info
 
     else: 
